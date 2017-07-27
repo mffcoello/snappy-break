e87c8ed577af88956fa24e0271aec79e526e4743
Katherine Sebina <katherinesebina@gmail.com>
2017-07-27 08:17:26 -0700

    Added the Need A Break button

---
+++ javascript/pop-up.js
@@ -0,0 +1,25 @@
+function showdiv() {
+  document.getElementById('pop').style.display = 'none';
+  var checked = document.querySelector('input[name="pop-up"]:checked').value;
+  document.getElementById('vid').style.display = 'block';
+  if (checked === "videos"){
+    randomVideos();
+  }
+  if (checked === "memes"){
+    randomMemes();
+  }
+
+}
+function randomVideos(){
+  var videos = ['https://www.youtube.com/embed/3v5HZTwFBCs',
+  'https://www.youtube.com/embed/CTFtOOh47oo',
+  'https://www.youtube.com/embed/by3yRdlQvzs'];
+  document.getElementById('iframe').src = videos[Math.floor(Math.random()*videos.length)];
+
+
+}
+
+function randomMemes(){
+ var memes = [ 'https://giphy.com/embed/dcIW9oEyeFePm'];
+ document.getElementById('iframe').src = memes[Math.floor(Math.random()*memes.length)];
+}
